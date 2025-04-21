77d7d4b579fbadfd8ea7a3ae1e4b25f6d99e27b2
karri-durgaprasad <139992301+karri-durgaprasad@users.noreply.github.com>
2025-04-21 19:18:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import About from "./REDUXSETUP/About";
 function App(){
       return(
           <>
-          <BrowserRouter basename="/Reduxtwo">
+          <BrowserRouter>
           <Reduxprovider store={store}>
             <Routes>
-                <Route path="/" element={<Display/>}/>
+                <Route path="/Reduxtwo" element={<Display/>}/>
                 <Route path="show" element={<Show/>}/>
                 <Route path="about" element={<About />} />
             </Routes>
